d0edf72e087ff0bd36c345a416f7dd1fd9004ac2
Ryan Lee <ryanm.lee11@gmail.com>
2020-08-07 23:31:47 -0400

    Time Display JavaScript updates

--- app.js
+++ app.js
@@ -3,4 +3,22 @@ const app = () => {
     const play = document.querySelector('.play');
     const outline = document.querySelector('.moving-outline circle');
     const video = document.querySelector('.vid-container');
+
+
+    //Sounds
+    const sounds = document.querySelectorAll('.sound-picker button')
+
+    //Time Display
+    const timeDisplay = document.querySelector('.time-display');
+
+    //Get length of the outline
+    const outlineLength = outline.getTotalLength();
+    //Duration
+
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
 }
+
+app();
